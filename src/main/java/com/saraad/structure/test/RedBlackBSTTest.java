a9420a63ef9e83fb1456357c5c8293bf9ef28d52
saraad <saraadkun@gmail.com>
2022-08-03 23:37:29 +0800

    BST test

--- src/main/java/com/saraad/structure/test/RedBlackBSTTest.java
+++ src/main/java/com/saraad/structure/test/RedBlackBSTTest.java
@@ -1,8 +1,10 @@
 package com.saraad.structure.test;
 
 import com.saraad.structure.RedBlackBST;
+import org.junit.Assert;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class RedBlackBSTTest {
@@ -14,16 +16,20 @@ public class RedBlackBSTTest {
         }
         System.out.println(bst);
         System.out.println("======================================");
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        System.out.println(bst.size());
+        HashSet<Integer> set = new HashSet<Integer>();
+        int preSize = bst.size();
+        System.out.println(preSize);
+        System.out.println("======================================");
         for (int i = 0; i < 50; i++) {
             int n = (int)Math.floor(Math.random() * 200);
-            list.add(n);
+            set.add(n);
             bst.delete(n);
         }
         System.out.println(bst);
         System.out.println(bst.size());
-        System.out.println(list.toString());
+        System.out.println(set.toString());
+        System.out.println(set.size());
+        Assert.assertEquals(preSize - set.size(), bst.size());
     }
 
 
