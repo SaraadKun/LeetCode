b5bcd0b909b5262bdcfa601df1c969edd41234c6
saraad <saraadkun@gmail.com>
2022-09-04 01:35:15 +0800

    ArrayUtil

---
+++ src/main/java/com/saraad/leetcode/utils/ArrayUtil.java
@@ -0,0 +1,29 @@
+package com.saraad.leetcode.utils;
+
+import com.saraad.util.JSONUtil;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Description: desc
+ * @Author: Saraad
+ * @Link: url
+ * @Date: 04-09-2022 01:30
+ */
+
+public class ArrayUtil {
+
+    /**
+     * @Description: 二维数组字符串输入转换
+     * @param input 二维数组json字符串
+     * @return null如果input为空, 其他情况二维数组
+     */
+    public static int[][] mock(String input) {
+        return StringUtils.isEmpty(input) ? null : JSONUtil.readValue(input, int[][].class);
+    }
+
+    public static void main(String[] args) {
+        String input = "[[]]";
+        System.out.println(JSONUtil.writeValueAsString(mock(input)));
+    }
+
+}
