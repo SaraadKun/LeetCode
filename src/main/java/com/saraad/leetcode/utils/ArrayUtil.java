a4f9c6ebb87608203d146432ba92602994a57c23
saraad <saraadkun@gmail.com>
2022-09-05 12:37:03 +0800

    fixbug

--- src/main/java/com/saraad/leetcode/utils/ArrayUtil.java
+++ src/main/java/com/saraad/leetcode/utils/ArrayUtil.java
@@ -15,10 +15,10 @@ public class ArrayUtil {
     /**
      * @Description: 二维数组字符串输入转换
      * @param input 二维数组json字符串
-     * @return null如果input为空, 其他情况二维数组
+     * @return 二维int数组
      */
     public static int[][] mock(String input) {
-        return StringUtils.isEmpty(input) ? null : JSONUtil.readValue(input, int[][].class);
+        return StringUtils.isEmpty(input) ? new int[][]{} : JSONUtil.readValue(input, int[][].class);
     }
 
     public static void main(String[] args) {
