fa52a0481a07ce5d380b0fbb574caf2fbac37e07
saraad <saraadkun@gmail.com>
2022-09-10 01:44:08 +0800

    util

--- src/main/java/com/saraad/leetcode/utils/ArrayUtil.java
+++ src/main/java/com/saraad/leetcode/utils/ArrayUtil.java
@@ -17,13 +17,29 @@ public class ArrayUtil {
      * @param input 二维数组json字符串
      * @return 二维int数组
      */
-    public static int[][] mock(String input) {
+    public static int[][] mock2d(String input) {
         return StringUtils.isEmpty(input) ? new int[][]{} : JSONUtil.readValue(input, int[][].class);
+    }
+    /**
+     * @Description: 一维数组字符串输入转换
+     * @param input 一维数组json字符串
+     * @return int数组
+     */
+    public static int[] mock1d(String input) {
+        return StringUtils.isEmpty(input) ? new int[]{} : JSONUtil.readValue(input, int[].class);
+    }
+    /**
+     * @Description: 一维String数组字符串输入转换
+     * @param input 一维String数组json字符串
+     * @return String数组
+     */
+    public static String[] mockStringArray(String input) {
+        return StringUtils.isEmpty(input) ? new String[]{} : JSONUtil.readValue(input, String[].class);
     }
 
     public static void main(String[] args) {
         String input = "[[]]";
-        System.out.println(JSONUtil.writeValueAsString(mock(input)));
+        System.out.println(JSONUtil.writeValueAsString(mock2d(input)));
     }
 
 }
